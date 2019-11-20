05a7d6716c7d934e31ef2e7f0bb966e3b05a5058
Achilles <achillesusuoyibo7@gmail.com>
2019-11-20 12:57:33 +0000

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="navbar__wrapper container-fluid">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <router-link to="/" class="logo"><img src="../assets/logo.png" alt=""></router-link>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                               <router-link to="/" class="nav-link">Home</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/" class="nav-link">About</router-link>
+                            </li>
+                             <li class="nav-item">
+                                <router-link to="/" class="nav-link">Decor shop</router-link>
+                            </li>
+                             <li class="nav-item">
+                                <router-link to="/" class="nav-link">Services</router-link>
+                            </li>
+                             <li class="nav-item">
+                                <router-link to="/" class="nav-link">Become an ambassador</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/" class="nav-link">Login</router-link>
+                            </li>
+                             <li class="nav-item">
+                                <router-link to="/" class="nav-link">Contact</router-link>
+                            </li>
+                            </ul>
+                        </div>
+                    </nav>
+                            </div>
+                        </div>
+</template>
+<style lang="scss" scoped>
+@import '../styles/colors';
+.navbar__wrapper{
+    padding: 1rem 0;
+    .nav-link{
+    color: #627081;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: .8rem;
+    margin-right: .7rem;
+}
+.fa-bars{
+    // background: $primary-color;
+    color: $primary-color;
+}
+}
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="topbar__wrapper container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <i class="fa fa-home"></i>&nbsp; 22 NDC Road, Beside Police Station, Ozoro, Delta State, Nigeira.
+                </div>
+                <div class="col-md-3">
+                    <i class="fa fa-envelope"></i>&nbsp; hello@primedecor.com
+                </div>
+                <div class="col-md-3">
+                    <i class="fa fa-phone"></i>&nbsp; +2348103515205
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Topbar'
+}
+</script>
+<style lang="scss" scoped>
+@import '../styles/colors';
+.topbar__wrapper{
+    background: $primary-color;
+    color: #fff;
+    font-size: .83rem;
+    padding: .9rem 0;
+}
+</style>
